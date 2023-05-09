d96be4a4af93461a985d3b9dd2a828445c6756b7
@gustin <agufus91@gmail.com>
2023-05-09 01:28:40 -0300

    docker

---
+++ src/main/java/com/portfolio/afunes/Entity/Persona.java
@@ -0,0 +1,220 @@
+package com.portfolio.afunes.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+public class Persona {
+    
+    //nombre, apellido, edad, nacionalidad, estadocivil, titulo, domicilio, email, telefono, fb, ig, ds, gh, img
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String nombre;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String apellido;
+    
+    @NotNull
+    private int edad;
+    
+    @Size(min = 1, max = 1500, message = "No cumple con la longitud")
+    private String descripcion;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String nacionalidad;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String estadoCivil;
+    
+    @Size(min = 1, max = 100, message = "No cumple con la longitud")
+    private String titulo;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String domicilio;
+    
+    @NotNull
+    @Size(min = 1, max = 100, message = "No cumple con la longitud")
+    private String email;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String telefono;
+    
+    @Size(min = 1, max = 100, message = "No cumple con la longitud")
+    private String facebook;
+    
+    @Size(min = 1, max = 100, message = "No cumple con la longitud")
+    private String instagram;
+    
+    @Size(min = 1, max = 100, message = "No cumple con la longitud")
+    private String discord;
+    
+    @Size(min = 1, max = 100, message = "No cumple con la longitud")
+    private String github;
+    
+    private String img;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, int edad, String descripcion, String nacionalidad, String estadoCivil, String titulo, String domicilio, String email, String telefono, String facebook, String instagram, String discord, String github, String img) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.descripcion = descripcion;
+        this.nacionalidad = nacionalidad;
+        this.estadoCivil = estadoCivil;
+        this.titulo = titulo;
+        this.domicilio = domicilio;
+        this.email = email;
+        this.telefono = telefono;
+        this.facebook = facebook;
+        this.instagram = instagram;
+        this.discord = discord;
+        this.github = github;
+        this.img = img;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getDiscord() {
+        return discord;
+    }
+
+    public void setDiscord(String discord) {
+        this.discord = discord;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+    
+    
+}
